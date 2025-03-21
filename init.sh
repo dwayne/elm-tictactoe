f7f0d07d02577e8677548f2ad35d302d6d63e3e7
Dwayne Crooks <me@dwaynecrooks.com>
2025-03-20 21:22:51 -0400

    Rewrite the production build script

--- init.sh
+++ init.sh
@@ -7,7 +7,3 @@ serve () {
 
   caddy file-server --browse --listen :"$port" --root "$project/public"
 }
-
-build-elm () {
-  elm make "$project/src/Main.elm" --debug --output="$project/public/app.js"
-}
